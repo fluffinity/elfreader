f30b6c2e9677229de42b286ca8daec44cb46dba3
fluffyChaos <23279594+fluffychaos@users.noreply.github.com>
2022-05-31 10:11:50 +0200

    added more --help documentation added more information to cargo.toml

--- src/cli.rs
+++ src/cli.rs
@@ -1,23 +1,25 @@
 use std::path::PathBuf;
 
 /// elfreader is a small tool to read the metadata of binary files in the ELF format.
-/// This includes the architecture the code is for, whether it is 32- or 64-bits etc.
+/// This includes the architecture the code is for, whether it is 32- or 64-bits,
+/// the endianness of the code and data, the file type of this binary, the ABI it uses
+/// and other metedata required by the linker or OS to properly load the binary.
 #[derive(clap::Parser)]
-#[clap(author, version, about, long_about = None)]
+#[clap(version, about, long_about = None)]
 pub struct Arguments {
     /// The path to the ELF file
     #[clap(parse(from_os_str))]
     pub path: PathBuf,
 
     /// Print the ELF header
-    #[clap(short, long = "program-header")]
+    #[clap(short, long)]
     pub program_header: bool,
 
     /// Print the program headers
-    #[clap(short, long = "section-header")]
+    #[clap(short, long)]
     pub section_header: bool,
 
     /// Print the section headers
-    #[clap(short, long = "header")]
+    #[clap(long)]
     pub header: bool,
 }
